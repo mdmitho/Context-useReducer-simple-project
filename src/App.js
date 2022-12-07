02c3e96b0a67942ba4f9e3d938a82ebebffba9c0
Awlad Hossen <mdawladhossenmito199922@gmail.com>
2022-12-07 13:02:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useEffect, useState } from 'react';
+import { RouterProvider } from 'react-router-dom';
+import router from './components/Routes/Routes';
 
+  const PRODUCT_CONTEXT = createContext()
 function App() {
+
+const [data, setData] = useState([])
+console.log(data)
+useEffect(() =>{
+  fetch("http://localhost:5000/watch")
+  .then((res)=> res.json())
+  .then((data)=> setData(data))
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto' >
+     <RouterProvider router={router}/>
     </div>
   );
 }
---
+++ src/components/page/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Navbar from './Navbar'
+
+export const Home = () => {
+  return (<>
+  
+  <Navbar/>
+
+  </>
+    
+  )
+}
